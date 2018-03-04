af5cf585bdc53ae6f037cde72e090364f1201359
mwafa <wafax.4@gmail.com>
2018-03-04 19:28:28 +0700

    test

--- py/joko.py
+++ py/joko.py
@@ -1,8 +1,6 @@
-import cv2
 import matplotlib.pyplot as plt
 
-image = cv2.imread("../image/mesi.jpg")
-cv2.imshow("hore",image)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+x = range(12)
+plt.plot(x)
+plt.show
