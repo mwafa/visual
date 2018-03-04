5257ea6f1861744dfd9d01fa1502d3dae93ef2c2
mwafa <wafax.4@gmail.com>
2018-03-04 21:15:12 +0700

    update terbaru

--- py/kuliah-1.py
+++ py/kuliah-1.py
@@ -1,4 +1,11 @@
 import cv2 as cv
 
-mesi = cv.imgopen("../image/mesi.jpg")
-cv.imshow(mesi)
+
+mesi = cv.imread("../image/mesi.jpg")
+
+mesi = cv.GaussianBlur(mesi, (35, 1), 0)
+mesi = cv.cvtColor(mesi,cv.COLOR_RGB2GRAY)
+
+cv.imshow("coba", mesi)
+cv.waitKey(0)
+cv.destroyAllWindows()
