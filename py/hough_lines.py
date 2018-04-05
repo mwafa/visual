9ad91ea82a2a8ca7cb70dd6737ef76f7b348a08e
AhmadUlilRider <36322180+AhmadUlilRider@users.noreply.github.com>
2018-04-05 09:34:40 +0800

    Update hough_lines.py

--- py/hough_lines.py
+++ py/hough_lines.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('perfectaction.jpg')
+img = cv2.imread(../'perfectaction.jpg')
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray,50,120)
 minLineLength = 20
