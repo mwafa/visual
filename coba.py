e1aa32221c5da67b086721f4a01de3b008b3a56c
mwafa <wafax.4@gmail.com>
2018-04-12 13:23:49 +0700

    new

--- coba.py
+++ coba.py
@@ -1,4 +1,11 @@
-from visTF.uts import bulat
+# from visTF.uts import bulat
+import cv2
+# import matplotlib.pyplot as plt
+# import numpy as np
 
-a = bulat('example/image/example.jpg')
-a.original()
+a = cv2.imread('example/image/kei.jpg')
+cv2.circle(a,(20,20),5,(255,255,255),2)
+print(a.shape)
+# cv2.imshow('nama',a)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
