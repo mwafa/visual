7b4a774272baa24a5da22daf7bbbf5d6eb29b76c
mwafa <wafax.4@gmail.com>
2018-06-03 12:51:05 +0700

    penyederhanaan

--- py/tebak_angka.py
+++ py/tebak_angka.py
@@ -1,8 +1,6 @@
 tebakan = 0
 for k in range(5):
     i = 2**k
-    data = [j for j in range(32) if (j//i)%2 == 1]
-    print(data)
-    a = input("ada disini?[Y/N]")
-    tebakan = tebakan + i if a in ["y","Y"] else tebakan
+    print([j for j in range(32) if (j//i)%2 == 1])
+    tebakan = tebakan + i if input("ada disini?[Y/N]") in ["y","Y"] else tebakan
 print(tebakan)
